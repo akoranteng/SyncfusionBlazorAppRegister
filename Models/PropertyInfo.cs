c7211b2322853fc821cd25ee11d6ed19a868175a
aKoranteng <akoranteng@msn.com>
2025-09-29 17:02:53 -0400

    Add PropertyInfo model for Card views

---
+++ Models/PropertyInfo.cs
@@ -0,0 +1,20 @@
+namespace SyncfusionBlazorAppRegister.Models
+{
+	public class PropertyInfo
+	{
+		// Properties go here
+		public int Id { get; set; }
+		public string? Title { get; set; }
+		public string? Description { get; set; }
+		public string? City { get; set; }
+		public string? Type { get; set; }
+		public decimal Price { get; set; }
+		public int Bedrooms { get; set; }
+		public int Bathrooms { get; set; }
+		public string? ImageUrl { get; set; }
+		public string VideoUrl { get; set; }
+		public DateTime ListedDate { get; set; }
+		public bool IsFeatured { get; set; }
+
+	}
+}
