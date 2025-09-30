47a4c6ed0eef42b675b251777e2b7c56ec2dea69
aKoranteng <akoranteng@msn.com>
2025-09-30 11:13:09 -0400

    Add mock PropertyService and register in Program.cs

---
+++ Services/PropertyService.cs
@@ -0,0 +1,30 @@
+﻿using SyncfusionBlazorAppRegister.Models;
+
+namespace SyncfusionBlazorAppRegister.Services
+{
+    public class PropertyService
+    {
+        public List<PropertyInfo> GetProperties()
+        {
+            return new List<PropertyInfo>
+            {
+                new PropertyInfo
+                {
+                    Id = 1,
+                    Title = "Modern Loft in Boston",
+                    Description = "Spacious loft with skyline views.",
+                    City = "Boston",
+                    Type = "Loft",
+                    Price = 850000,
+                    Bedrooms = 2,
+                    Bathrooms = 2,
+                    ImageUrl = "images/loft1.jpg",
+                    VideoUrl = "videos/loft1.mp4",
+                    ListedDate = DateTime.Now.AddDays(-10),
+                    IsFeatured = true
+                },
+                // Add more mock listings here
+            };
+        }
+    }
+}
